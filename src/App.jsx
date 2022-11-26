edd2300e569d15f4b9e067bd1315447ba9e1d6bb
zarriy <zarriy@outlook.com>
2022-11-26 13:12:13 +0500

    commiting the starting files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import HomePage from "./home";
+import Question from "./questions";
+
+function App() {
+  return (
+    <div className="app-div">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>Quizzical</h1>
+      <p>Test you abilities, become aware of your limitations</p>
+      <button>Start Game</button>
+    </div>
+  );
+}
+export default HomePage;
